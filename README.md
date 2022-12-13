452c3675babecd2432df12c20337bc2a929971fb
mrsjlwhite <jlwhite.codes@gmail.com>
2022-12-12 23:33:46 -0800

    📝 Updated readMe, added site screenshot, updated footer

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # A Portfolio Template
 
-An art portfolio template. 🪴🖼️
+An art portfolio template for Candi. 💜🪴🖼️
+
+[![pages-build-deployment](https://github.com/mrsjlwhite/hundekeycreeates/actions/workflows/pages/pages-build-deployment/badge.svg)](https://github.com/mrsjlwhite/hundekeycreeates/actions/workflows/pages/pages-build-deployment)
+
+[Site screenshot](./build/images/site-screenshot2022-12-12.png)
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,15 +3,17 @@ import './_footer.scss';
 
 function Footer() {
     const [currentYear, setYear] = useState();
-    
+
     useEffect(() => {
-        const d = new Date(); 
+        const d = new Date();
         setYear(d.getFullYear());
     }, [])
-        
+
     return (
         <footer className='footer'>
-            Copyright &#169; {currentYear} Candace Jackson.
+            Copyright &#169; {currentYear} Candace Jackson
+            &#38;
+            <a target="_blank" href='https://github.com/mrsjlwhite' rel="noreferrer"> Jeslyn White</a>
         </footer>
     )
 }
