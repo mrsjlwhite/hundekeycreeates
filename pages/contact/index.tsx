53c5734c04612343555bd48367146160c68e5718
Jeslyn <75355441+mrsjlwhite@users.noreply.github.com>
2024-01-16 18:28:26 -0800

    📱 Fixes contact page mobile layout

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,6 +1,6 @@
 import Styles from './_contact.module.scss';
 import { ContactPage } from "../../data/contactPage";
-import { Container } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
@@ -11,25 +11,27 @@ export default function Contact() {
 
     return (
         <Container className={Styles.contactContainer}>
-            <h3 className={Styles.contactTitle}>
-                Contact Me
-            </h3>
-            <h5 className={Styles.contactDesc}>
-                {ContactPage.description}
-                {ContactPage.instagrams.map(ig => {
-                    return (
-                        <div key={ig.handle}
-                            className={Styles.contactLink}
-                            onClick={() => onIconClick(ig.link)}>
-                            <FontAwesomeIcon icon={faInstagram} size={'2xl'} />
-                            <p>
-                                &nbsp;
-                                {ig.handle}
-                            </p>
-                        </div>
-                    )
-                })}
-            </h5>
+            <Row>
+                <h3 className={Styles.contactTitle}>
+                    Contact Me
+                </h3>
+                <h5 className={Styles.contactDesc}>
+                    {ContactPage.description}
+                    {ContactPage.instagrams.map(ig => {
+                        return (
+                            <div key={ig.handle}
+                                className={Styles.contactLink}
+                                onClick={() => onIconClick(ig.link)}>
+                                <FontAwesomeIcon icon={faInstagram} size={'2xl'} />
+                                <p>
+                                    &nbsp;
+                                    {ig.handle}
+                                </p>
+                            </div>
+                        )
+                    })}
+                </h5>
+            </Row>
         </Container>
     )
 };
